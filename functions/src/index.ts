a1f89eb40e94bd57748706118a3cf05dda3608d9
Christian Loosli <loosli.christian@gmail.com>
2020-05-24 23:28:08 -0600

    implement proxy function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,31 @@
 import * as functions from 'firebase-functions';
+const http = require('requestify');
+const cors = require('cors')({ origin: true });
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 
-export const weahterForecast = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
+export const weahterForecastProxy = functions.https.onRequest((request, response) => {
+    cors(request, response, () => {
+        /// Get the url params
+        const lat = request.query.lat;
+        const lng = request.query.lng;
+
+        const url = formatUrl(lat, lng);
+
+        /// Send request to DarkSky
+        return http.get(url).then((res: any) => {
+            return res.status(200).send(res.getBody());
+        }).catch((err: any) => {
+            return response.status(400).send(err)
+        })
+    });
 });
+
+/// Helper to format the request URL
+function formatUrl(lat: any, lng: any) {
+    const apiKey = functions.config().openweatherapi.key
+    console.log(apiKey)
+
+    return `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lng}&cnt=7&appid=${apiKey}`
+}
